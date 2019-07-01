0e7e376d3411790cce3d65e687d954141f390df7
sophiac15 <sophiaclark@college.harvard.edu>
2019-07-01 16:13:32 -0700

    almost done w mvp

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,28 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Tweet {
+
+    // list out the attributes
+    public String body;
+    public long uid;
+    public User user;
+    public String createdAt;
+
+    // deserialize the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        // extract the values from JSON
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("uid");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+
+        return tweet;
+    }
+
+
+}
